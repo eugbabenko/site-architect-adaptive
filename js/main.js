e9fb2feae9407237adee2d53897b579ad39428e2
Eugene <eug.babenko@gmail.com>
2022-06-07 09:24:48 +0300

    master

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.image__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 1000
+      });
+});
