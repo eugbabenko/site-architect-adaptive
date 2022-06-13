4571dd0a4ee63206b0c344f18a8bf0c2f250f66c
Eugene Babenko <eug.babenko@gmail.com>
2022-06-13 18:25:03 +0300

    Fix slider for adaptive

--- js/main.js
+++ js/main.js
@@ -2,8 +2,24 @@ $(function(){
     $('.image__slider').slick({
         arrows: false,
         dots: true,
+        infinite: false,
         slidesToShow: 3,
         slidesToScroll: 3,
-        speed: 1000
-      });
-});
+        speed: 1000,
+        responsive: [
+          {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+          {
+            breakpoint: 850,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }]
+    })
+  })
